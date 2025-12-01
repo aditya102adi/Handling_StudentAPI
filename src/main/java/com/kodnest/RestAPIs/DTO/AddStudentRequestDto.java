ac9c87cc748ed4a60617675fb7662b151db2403a
aditya102adi <aditya102adi@gmail.com>
2025-12-01 16:08:41 +0530

    Request Validation

--- src/main/java/com/kodnest/RestAPIs/DTO/AddStudentRequestDto.java
+++ src/main/java/com/kodnest/RestAPIs/DTO/AddStudentRequestDto.java
@@ -1,12 +1,22 @@
 package com.kodnest.RestAPIs.DTO;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class AddStudentRequestDto {
+    @NotBlank(message = "Name is Required")
+    @Size(min = 3, max = 30, message = "Name should be min 3 and max 3 characters")
     private String name;
+
+
+    @Email
+    @NotBlank(message = "Email is Required")
     private String email;
+
 
     public AddStudentRequestDto(String name, String email) {
         this.name = name;
